b6f8a369947b97e5c65f8a277355f2c228d58196
Ascellayn <pro.siriusb@gmail.com>
2024-08-04 00:33:48 +0000

    i have no fucking clue what i did it's 00:32:50 CEST on the 4th of august 2024, there's a fucking big ass party or concert idfk in the city next to mine and it's loud as fuck and i can't sleep and it's too hot everywhere :(

--- SN-API_Client.js
+++ SN-API_Client.js
@@ -1,8 +1,8 @@
 /* Sirio Network API*/
 function SirioAPI(API_Request) {
     return new Promise(function(Success) {
-        const Trinity = new WebSocket("wss://trinity.sirio-network.com");
-        // const Trinity = new WebSocket("ws://localhost:1407");
+        // const Trinity = new WebSocket("wss://trinity.sirio-network.com");
+         const Trinity = new WebSocket("wss://localhost");
         Trinity.onopen = function(e) {
             log(`[SN-API] Sending Request "${API_Request}"`)
             Trinity.send(API_Request);
@@ -10,16 +10,20 @@ function SirioAPI(API_Request) {
 
         Trinity.onmessage = function(event) {
             Success(event.data);
-        }
+        };
 
         Trinity.onclose = function(event) {
             if (event.wasClean) {
                 log(`[SN-API] Connection closed. Code=${event.code} Reason=${event.reason}`);
-                return "Fuck"
+                return "CLOSED";
             } else {
                 log("[SN-API] Connection closed forcefully.");
-                return "Fuck"
-            }
-        }
+                return "CLOSED_FORCE";
+            };
+        };
+
+        Trinity.onerror = function(error) {
+            log("[SN-API] Unknown error")
+        };
     });
 };
